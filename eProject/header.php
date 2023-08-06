1844c0d64c844dea386c27ada2307abcb3d05132
Iqra <iqra44938@gmail.com>
2023-08-06 19:52:02 +0500

    commited

--- eProject/header.php
+++ eProject/header.php
@@ -66,11 +66,7 @@ foreach ($user as $value) {
         <div class="sidebar pe-4 pb-3">
             <nav class="navbar bg-light navbar-light">
                 <a href="index.php" class="navbar-brand mx-4 mb-3">
-<<<<<<< HEAD
                     <h6 class="text-primary"><i class="fa fa-hashtag me-2"></i>VaccinationBookingSystem</h6>
-=======
-                    <h6 class="text-primary"><i class="fa fa-hashtag me-2"></i>Vaccination booking system</h6>
->>>>>>> 0064da51d7ed45722c05fd76da48fa13c0f3d75c
                 </a>
 
                 <div class="d-flex align-items-center ms-4 mb-4">
